ba80b6e942b47dcb147b8ac6a2a6529b52f57e6f
everythingfades <raymondguhonglei@163.com>
2025-12-26 08:50:59 +0000

    fix: prepared two set of env vars

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_PUBLISHABLE_KEY
+const supabaseUrl = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
 console.log(supabaseKey, supabaseUrl)
 
 if (!supabaseUrl || !supabaseKey) {
