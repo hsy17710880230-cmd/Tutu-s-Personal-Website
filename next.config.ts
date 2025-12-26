fd7e004649a093d6274809d78789333dea7eea35
everythingfades <raymondguhonglei@163.com>
2025-12-26 08:48:51 +0000

    fix: dist folder

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: 'dist',  // changes the build output directory (optional)
+  // distDir: 'dist',  // changes the build output directory (optional)
 };
 
 export default nextConfig;
